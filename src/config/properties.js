c8aaa346ef702715d35b0aa90ae8fc97450c9f68
Dikshit Thakral <dikshitthakral@Dikshits-MacBook-Pro.local>
2023-04-12 12:19:08 +0530

    Added bucket info

--- src/config/properties.js
+++ src/config/properties.js
@@ -1,8 +1,8 @@
 module.exports = {
     PORT : 8080,
     DB : 'mongodb+srv://test:test@cluster0.h7sxi.mongodb.net/erp_school_db?retryWrites=true&w=majority',
-    AWS_S3_SECRET_ACCESS_KEY : '',
-    AWS_S3_ACCESS_KEY_ID : '',
-    AWS_S3_REGION : '',
-    AWS_S3_BUCKET : '' 
+    AWS_S3_SECRET_ACCESS_KEY : '7TBQECLP1hZl80HRVzZgBrM4/VLIPL0RcRgTLR/H',
+    AWS_S3_ACCESS_KEY_ID : 'AKIAYL3QKZ4LXIU2JUKN',
+    AWS_S3_REGION : 'ap-northeast-1',
+    AWS_S3_BUCKET : 'test-cloud-storage' 
 }
