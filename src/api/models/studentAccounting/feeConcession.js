0669fd151ed73103ed27120db13077a41e559c47
Kishan Tripathi <64527748+Kishan85@users.noreply.github.com>
2023-05-31 17:18:56 +0530

    Update feeConcession.js

--- src/api/models/studentAccounting/feeConcession.js
+++ src/api/models/studentAccounting/feeConcession.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 
 const feeConcessionSchema = new mongoose.Schema({
-  invoiceNo: {
-    type: String,
-    required: true,
-  },
   studentId: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
