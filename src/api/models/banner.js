d677e13ec44c5a34fdae753e01e2f244b0c3db78
Kishan Tripathi <64527748+Kishan85@users.noreply.github.com>
2023-06-04 00:09:54 +0530

    Update banner.js

--- src/api/models/banner.js
+++ src/api/models/banner.js
@@ -1,15 +1,24 @@
 const mongoose = require("mongoose");
 
 const bannerSchema = new mongoose.Schema({
-    bannerName: {
-        type: String, default:"", required: true
-    },
-    bannerImage: {
-        type: String, default:"", required: true
-    },
-    createdAt: { type: Date, default: Date.now },
+  bannerType: {
+    type: String,
+    required: true,
+    enum: ["guardian", "teacher"],
+  },
+  bannerName: {
+    type: String,
+    default: "",
+    required: true,
+  },
+  bannerImage: {
+    type: String,
+    default: "",
+    required: true,
+  },
+  createdAt: { type: Date, default: Date.now },
 });
-  
+
 const bannerModal = new mongoose.model("Banner", bannerSchema);
-  
+
 module.exports = bannerModal;
