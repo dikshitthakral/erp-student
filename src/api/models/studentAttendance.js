64e831483dc26ae9d4e1def06006a6af71e6a1a5
Kishan Tripathi <64527748+Kishan85@users.noreply.github.com>
2023-06-20 01:37:14 +0530

    Add files via upload

---
+++ src/api/models/studentAttendance.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const studentSchema = new mongoose.Schema(
+  {
+    student: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Student",
+      required: true,
+    },
+  },
+  { autoIndex: false, autoCreate: false }
+);
+
+const studentaAttendanceSchema = new mongoose.Schema(
+  {
+    classId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Class",
+      required: true,
+    },
+    sectionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Section",
+      required: true,
+    },
+    year: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["fullDay", "halfDay", "absent"],
+      required: true,
+    },
+    date: {
+      type: String,
+      default:"",
+    },
+    students: [studentSchema],
+  },
+  (createAt = { timestamps: true, autoIndex: false, autoCreate: false })
+);
+
+const studentAttendance = new mongoose.model(
+  "StudentAttendance",
+  studentaAttendanceSchema
+);
+
+module.exports = studentAttendance;
