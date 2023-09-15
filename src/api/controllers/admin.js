7c212ff27c4bf86cfb92349b140d151010facc73
Stpl-pradeep <120784865+Stpl-pradeep@users.noreply.github.com>
2023-09-15 20:29:38 +0530

    Update admin.js

--- src/api/controllers/admin.js
+++ src/api/controllers/admin.js
@@ -1,10 +1,19 @@
 const student = require('../models/students');
 const raiseTicket = require('../models/raiseATicket');
 const vehicle = require('../models/transport/vehicle');
+const route = require('../models/transport/route');
 const employee = require('../models/employee');
 const classCount = require('../models/class');
 const section = require('../models/section');
+const studentAttandace = require('../models/studentAttendance');
+const employeeAttandace = require('../models/employeeAttendance');
+const visitorLog = require('../models/reception/visitoryLog');
+const callLog = require('../models/reception/callLog');
+const enquiry = require('../models/reception/enquiry');
+const banner = require('../models/banner');
+const noticeBoard = require('../models/noticeBoard');
 const mongoose = require('mongoose');
+
 
 const getAll = async (req, res) => {
     try {
@@ -49,7 +58,7 @@ const getAll = async (req, res) => {
 }
 
 
-const getTickets = async (req, res) => {    
+const getTickets = async (req, res) => {
     try {
         let tickets = await raiseTicket.find();
         if (
@@ -83,32 +92,88 @@ const getTickets = async (req, res) => {
 
 const getVehicleCount = async (req, res) => {
     try {
-        let vehicles = await vehicle.find();
-        if (
-            vehicles !== undefined &&
-            vehicles.length !== 0 &&
-            vehicles !== null
-        ) {
-            const vehicleCount = vehicles?.length;
-            return res.status(200).send({
-                vehicleCount: vehicleCount,
-                message: `Total vehicles: ${vehicleCount}`,
-                success: true,
-            });
-        } else {
-            return res.status(200).send({
-                vehicleCount: 0,
-                message: "No vehicles found",
-                success: true,
-            });
-        }
+        const vehicles = await vehicle.find();
+        const routes = await route.find();
+
+        const vehicleCount = vehicles.length;
+        const routeCount = routes.length;
+
+        return res.status(200).send({
+            vehicleCount,
+            routeCount,
+            message: vehicles.length > 0 ? 'All data' : 'No data found',
+            success: true,
+        });
     } catch (error) {
         return res.status(400).send({
-            message: "Something went wrong",
+            message: 'Something went wrong',
             success: false,
         });
     }
-}
+};
+
+const getStudentEmpAttandance = async (req, res) => {
+    try {
+        const studentAtt = await studentAttandace.find();
+        const employeeAtt = await employeeAttandace.find();
+
+        const student = studentAtt.length ? studentAtt.length : 0;
+        const employee = employeeAtt.length ? employeeAtt.length : 0;
+
+        return res.status(200).send({
+            student,
+            employee,
+            message: 'data found',
+            success: true,
+        });
+    } catch (error) {
+        return res.status(400).send({
+            message: 'Something went wrong',
+            success: false,
+        });
+    }
+};
+
+const allReceptionCount = async (req, res) => {
+    try {
+      const [
+        visitorLogs,
+        callLogs,
+        enquirys,
+        banners,
+        noticeBoards,
+      ] = await Promise.all([
+        visitorLog.find(),
+        callLog.find(),
+        enquiry.find(),
+        banner.find(),
+        noticeBoard.find(),
+      ]);
+  
+      const visitorCount = visitorLogs.length ? visitorLogs.length : 0;
+      const callLogCount = callLogs.length ? callLogs.length : 0;
+      const enquiryCount = enquirys.length ? enquirys.length : 0;
+      const bannerCount = banners.length ? banners.length : 0;
+      const noticeBoardCount = noticeBoards.length ? noticeBoards.length : 0;
+  
+      return res.status(200).send({
+        visitorCount,
+        callLogCount,
+        enquiryCount,
+        bannerCount,
+        noticeBoardCount,
+        message: 'data found',
+        success: true,
+      });
+    } catch (error) {
+      return res.status(400).send({
+        message: 'Something went wrong',
+        success: false,
+      });
+    }
+  };
+  
+
 
 const getClassSectionCount = async (req, res) => {
     try {
@@ -140,5 +205,35 @@ const getClassSectionCount = async (req, res) => {
         });
     }
 }
+const totalVehicleAndROute = async (req, res) => {
+    try {
+        let classes = await classCount.find();
+        let sections = await section.find();
+        if (
+            classes !== undefined &&
+            classes.length !== 0 &&
+            classes !== null
+        ) {
+            const classCount = classes?.length;
+            const sectionCount = sections?.length;
+            return res.status(200).send({
+                classCount: classCount,
+                sectionCount: sectionCount,
+                message: `Total classes: ${classCount}, Total sections: ${sectionCount}`,
+                success: true,
+            });
+        } else {
+            return res.status(200).send({
+                message: "No classes or sections found",
+                success: true,
+            });
+        }
+    } catch (error) {
+        return res.status(400).send({
+            message: "Something went wrong",
+            success: false,
+        });
+    }
+}
 
-module.exports = { getAll,getTickets,getVehicleCount,getClassSectionCount };
+module.exports = { getAll, getTickets, getVehicleCount, getClassSectionCount,getStudentEmpAttandance,allReceptionCount };
