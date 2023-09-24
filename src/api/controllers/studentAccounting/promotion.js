0182a901472f7f93881e09b38447fb46bca8e1ee
Stpl-pradeep <120784865+Stpl-pradeep@users.noreply.github.com>
2023-09-25 00:52:28 +0530

    Update promotion.js

--- src/api/controllers/studentAccounting/promotion.js
+++ src/api/controllers/studentAccounting/promotion.js
@@ -1,241 +1,3 @@
-// const feeGroup = require("../../models/studentAccounting/feeGroup");
-// const mongoose = require("mongoose");
-// const academicsService = require("../../services/academic");
-// const { isEmpty, find } = require("lodash");
-// const academics = require("../../models/academic");
-// const students = require("../../models/students");
-// const feeConcession = require("../../models/studentAccounting/feeConcession");
-// const academicFeeType = require("../../models/studentAccounting/academicFeeType");
-
-// const promoteAll = async (req, res) => {
-//   try {
-//     const {
-//       academicYear,
-//       studentClass,
-//       section,
-//       promoteacademicYear,
-//       promotestudentClass,
-//       promoteSection,
-//     } = req.body;
-//     if (
-//       isEmpty(academicYear) ||
-//       isEmpty(
-//         studentClass ||
-//           isEmpty(section) ||
-//           isEmpty(promoteacademicYear) ||
-//           isEmpty(promotestudentClass) ||
-//           isEmpty(promoteSection)
-//       )
-//     ) {
-//       return res.status(400).send({
-//         messge: "Mandatory fields missing while promoting all students.",
-//         success: false,
-//       });
-//     }
-//     const findAcademic = await academicsService.getIdIfAcademicExists({
-//       academicYear,
-//       studentClass,
-//       section,
-//     });
-//     if (findAcademic === null) {
-//       return res.status(400).send({
-//         messge: "Academic not found.",
-//         success: false,
-//       });
-//     }
-//     const updateAcademic = await academicsService.getIdIfAcademicExists({
-//       academicYear: promoteacademicYear,
-//       studentClass: promotestudentClass,
-//       section: promoteSection,
-//     });
-//     if (updateAcademic === null) {
-//       return res.status(400).send({
-//         messge: "Update Academic not found.",
-//         success: false,
-//       });
-//     }
-//     const findStudent = await students.find({ academic: findAcademic._id });
-//     const academicFee = await academicFeeType.find({
-//       class: studentClass,
-//       year: academicYear,
-//     });
-//     findStudent.map(async (student) => {
-//       const findFeeConcession = await feeConcession.find({
-//         studentId: student._id,
-//         academicYear: academicYear,
-//       });
-//       const totalAmount = findFeeConcession[0]?.allFee[0]?.totalAmount;
-//       const totalFinalAmount = totalAmount + (totalAmount * 10) / 100;
-//       const invoice = Math.floor(100000 + Math.random() * 900000);
-//       const newFeeConcession = new feeConcession({
-//         invoiceNo: invoice,
-//         studentId: student._id,
-//         feemode: null,
-//         academicYear: promoteacademicYear,
-//         studentClass: promotestudentClass,
-//         totalFinalAmount: "",
-//         isEditableCategory: true,
-//         allFee: academicFee[0]?.feeCategory.map((fee) => {
-//           return {
-//             id: fee._id,
-//             categoryName: fee.categoryName,
-//             code: fee.code,
-//             amount:
-//               fee.categoryName === "AcademicFee"
-//                 ? totalFinalAmount
-//                 : fee.amount,
-//             concession: "",
-//             totalAmount: "",
-//             isChecked: fee.isChecked,
-//           };
-//         }),
-//         allMode: [],
-//       });
-//       if (
-//         findFeeConcession[0]?.studentId.toString() === student._id.toString()
-//       ) {
-//         await newFeeConcession.save();
-//       }
-//     });
-//     const updateStudent = await students.updateMany(
-//       { academic: findAcademic._id },
-//       {
-//         $set: {
-//           academic: updateAcademic._id,
-//           studentClass: promotestudentClass,
-//           section: promoteSection,
-//         },
-//       }
-//     );
-//     return res.status(200).json({
-//       updateStudent,
-//       message: "Students promoted successfully.",
-//       success: true,
-//     });
-//   } catch (error) {
-//     console.log(error.message);
-//     res.status(500).send({ message: error.message });
-//   }
-// };
-
-// const notPromoteAll = async (req, res) => {
-//   try {
-//     const {
-//       academicYear,
-//       studentClass,
-//       section,
-//       promoteacademicYear,
-//       promotestudentClass,
-//       promoteSection,
-//       studentId,
-//     } = req.body;
-//     if (
-//       isEmpty(academicYear) ||
-//       isEmpty(
-//         studentClass ||
-//           isEmpty(section) ||
-//           isEmpty(promoteacademicYear) ||
-//           isEmpty(promotestudentClass) ||
-//           isEmpty(promoteSection) ||
-//           isEmpty(studentId)
-//       )
-//     ) {
-//       return res.status(400).send({
-//         messge: "Mandatory fields missing while not promoting all students.",
-//         success: false,
-//       });
-//     }
-//     const findAcademic = await academicsService.getIdIfAcademicExists({
-//       academicYear,
-//       studentClass,
-//       section,
-//     });
-//     if (findAcademic === null) {
-//       return res.status(400).send({
-//         messge: "Academic not found.",
-//         success: false,
-//       });
-//     }
-//     const updateAcademic = await academicsService.getIdIfAcademicExists({
-//       academicYear: promoteacademicYear,
-//       studentClass: promotestudentClass,
-//       section: promoteSection,
-//     });
-//     if (updateAcademic === null) {
-//       return res.status(400).send({
-//         messge: "Update Academic not found.",
-//         success: false,
-//       });
-//     }
-//     const findStudent = await students.find({ academic: findAcademic._id });
-//     const academicFee = await academicFeeType.find({
-//       class: studentClass,
-//       year: academicYear,
-//     });
-//     findStudent.map(async (student) => {
-//       const findFeeConcession = await feeConcession.find({
-//         studentId: student._id,
-//         academicYear: academicYear,
-//       });
-//       const totalAmount = findFeeConcession[0]?.allFee[0]?.totalAmount;
-//       const totalFinalAmount = totalAmount + (totalAmount * 10) / 100;
-//       const invoice = Math.floor(100000 + Math.random() * 900000);
-//       const newFeeConcession = new feeConcession({
-//         invoiceNo: invoice,
-//         studentId: student._id,
-//         feemode: null,
-//         academicYear: promoteacademicYear,
-//         studentClass: promotestudentClass,
-//         totalFinalAmount: "",
-//         isEditableCategory: true,
-//         allFee: academicFee[0]?.feeCategory.map((fee) => {
-//           return {
-//             id: fee._id,
-//             categoryName: fee.categoryName,
-//             code: fee.code,
-//             amount:
-//               fee.categoryName === "AcademicFee"
-//                 ? totalFinalAmount
-//                 : fee.amount,
-//             concession: "",
-//             totalAmount: "",
-//             isChecked: fee.isChecked,
-//           };
-//         }),
-//         allMode: [],
-//       });
-//       if (
-//         findFeeConcession[0]?.studentId.toString() === student._id.toString()
-//       ) {
-//         await newFeeConcession.save();
-//       }
-//     });
-//     const student = await students.find({
-//       academic: findAcademic._id,
-//       _id: { $nin: studentId },
-//     });
-//     const updateStudent = await students.updateMany(
-//       { academic: findAcademic._id, _id: { $nin: studentId } },
-//       {
-//         $set: {
-//           academic: updateAcademic._id,
-//           studentClass: promotestudentClass,
-//           section: promoteSection,
-//         },
-//       }
-//     );
-//     return res.status(200).json({
-//       updateStudent,
-//       message: "Students promoted successfully.",
-//       success: true,
-//     });
-//   } catch (error) {
-//     console.log(error.message);
-//     res.status(500).send({ message: error.message });
-//   }
-// };
-
-// module.exports = { promoteAll, notPromoteAll };
 const feeGroup = require("../../models/studentAccounting/feeGroup");
 const mongoose = require("mongoose");
 const academicsService = require("../../services/academic");
@@ -244,6 +6,7 @@ const academics = require("../../models/academic");
 const students = require("../../models/students");
 const feeConcession = require("../../models/studentAccounting/feeConcession");
 const academicFeeType = require("../../models/studentAccounting/academicFeeType");
+const trackConcession = require("../../models/studentAccounting/trackConcession");
 
 const promoteAll = async (req, res) => {
   try {
@@ -259,10 +22,10 @@ const promoteAll = async (req, res) => {
       isEmpty(academicYear) ||
       isEmpty(
         studentClass ||
-          isEmpty(section) ||
-          isEmpty(promoteacademicYear) ||
-          isEmpty(promotestudentClass) ||
-          isEmpty(promoteSection)
+        isEmpty(section) ||
+        isEmpty(promoteacademicYear) ||
+        isEmpty(promotestudentClass) ||
+        isEmpty(promoteSection)
       )
     ) {
       return res.status(400).send({
@@ -302,9 +65,23 @@ const promoteAll = async (req, res) => {
         studentId: student._id,
         academicYear: academicYear,
       });
+      const fConcession = await trackConcession.find({
+        studentId: student._id
+      });
+
+
       const totalAmount = findFeeConcession[0]?.allFee[0]?.totalAmount;
       const totalFinalAmount = totalAmount + (totalAmount * 10) / 100;
-      console.log(totalAmount,totalFinalAmount,'===========================')
+      let promotionAmount = '0';
+      if (fConcession) {
+        const value = fConcession[0];
+        const finalConcession = value.amount + (value.amount * 10) / 100;
+        promotionAmount = finalConcession;
+        await trackConcession.updateOne(
+          { _id: value._id },
+          { $set: { amount: promotionAmount } }
+        );
+      }
       const invoice = Math.floor(100000 + Math.random() * 900000);
       const newFeeConcession = new feeConcession({
         invoiceNo: invoice,
@@ -312,16 +89,18 @@ const promoteAll = async (req, res) => {
         feemode: null,
         academicYear: promoteacademicYear,
         studentClass: promotestudentClass,
-        totalFinalAmount: "",
+        totalFinalAmount: promotionAmount,
         isEditableCategory: true,
         allFee: academicFee[0]?.feeCategory.map((fee) => {
           return {
             id: fee._id,
             categoryName: fee.categoryName,
             code: fee.code,
-            amount:fee.concession === "" ? totalAmount : totalFinalAmount,
+            // amount:fee.concession === "" ? totalAmount : totalFinalAmount,
+            amount: promotionAmount,
             concession: fee.concession ? fee.concession : "",
-            totalAmount: fee.concession === "" ? totalAmount : totalFinalAmount,
+            // totalAmount: fee.concession === "" ? totalAmount : totalFinalAmount,
+            totalAmount: promotionAmount,
             isChecked: fee.isChecked ? fee.isChecked : true,
           };
         }),
@@ -370,11 +149,11 @@ const notPromoteAll = async (req, res) => {
       isEmpty(academicYear) ||
       isEmpty(
         studentClass ||
-          isEmpty(section) ||
-          isEmpty(promoteacademicYear) ||
-          isEmpty(promotestudentClass) ||
-          isEmpty(promoteSection) ||
-          isEmpty(studentId)
+        isEmpty(section) ||
+        isEmpty(promoteacademicYear) ||
+        isEmpty(promotestudentClass) ||
+        isEmpty(promoteSection) ||
+        isEmpty(studentId)
       )
     ) {
       return res.status(400).send({
@@ -414,8 +193,20 @@ const notPromoteAll = async (req, res) => {
         studentId: student._id,
         academicYear: academicYear,
       });
+      const fConcession = await trackConcession.find({
+        studentId: student._id
+      });
       const totalAmount = findFeeConcession[0]?.allFee[0]?.totalAmount;
       const totalFinalAmount = totalAmount + (totalAmount * 10) / 100;
+      if (fConcession) {
+        const value = fConcession[0];
+        const finalConcession = value.amount + (value.amount * 10) / 100;
+        promotionAmount = finalConcession;
+        await trackConcession.updateOne(
+          { _id: value._id },
+          { $set: { amount: promotionAmount } }
+        );
+      }
       const invoice = Math.floor(100000 + Math.random() * 900000);
       const newFeeConcession = new feeConcession({
         invoiceNo: invoice,
@@ -423,16 +214,16 @@ const notPromoteAll = async (req, res) => {
         feemode: null,
         academicYear: promoteacademicYear,
         studentClass: promotestudentClass,
-        totalFinalAmount: "",
+        totalFinalAmount: promotionAmount,
         isEditableCategory: true,
         allFee: academicFee[0]?.feeCategory.map((fee) => {
           return {
             id: fee._id,
             categoryName: fee.categoryName,
             code: fee.code,
-            amount:fee.categoryName === "" ? totalAmount : totalFinalAmount,
+            amount: fee.categoryName === "" ? totalAmount : promotionAmount,
             concession: fee.concession,
-            totalAmount: fee.categoryName === "" ? totalAmount : totalFinalAmount,
+            totalAmount: fee.categoryName === "" ? totalAmount : promotionAmount,
             isChecked: fee.isChecked,
           };
         }),
@@ -470,4 +261,3 @@ const notPromoteAll = async (req, res) => {
 };
 
 module.exports = { promoteAll, notPromoteAll };
-
